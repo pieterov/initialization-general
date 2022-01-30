ee647f0f7e9d730dc522d63214592adb46efed0f
pieterov <pieter.overdevest@mac.com>
2022-01-30 16:51:14 +0100

    small update to install toolbox

--- M - Install latest General Toolbox.R
+++ M - Install latest General Toolbox.R
@@ -3,17 +3,17 @@
 # AUTHOR:       Pieter Overdevest
 ######################################################################################
 
-# If the package is attached to memory - i.e., there is a checkmark in front of the package
-# in the Packages tab - detach it from memory, first. In case the package is already detached,
-# then this code will result in a 'invalid name argument' error (not a problem).
-detach("package:generaltoolbox", unload = TRUE)
-
-# Update the toolbox by downloading the latest version from GitHub.
-devtools::install_github(
+        # If the package is attached to memory - i.e., there is a checkmark in front of the package
+        # in the Packages tab - detach it from memory, first. In case the package is already detached,
+        # then this code will result in a 'invalid name argument' error (not a problem).
+        detach("package:generaltoolbox", unload = TRUE)
         
-        rrepo        = "pieterov/generaltoolbox",
-        INSTALL_opts = c("--no-multiarch")
-)
-
-# Attach the (updated) package again to memory.
-library(generaltoolbox)
+        # Update the toolbox by downloading the latest version from GitHub.
+        devtools::install_github(
+                
+                rrepo        = "pieterov/generaltoolbox",
+                INSTALL_opts = c("--no-multiarch")
+        )
+        
+        # Attach the (updated) package again to memory.
+        library(generaltoolbox)
