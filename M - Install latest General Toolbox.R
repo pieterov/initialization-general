caeb967c56b78f87c202e5a6ce0a424ae3ecee50
pieterov <pieter.overdevest@mac.com>
2026-01-26 15:35:16 +0100

    cleaned up M - Install latest general toolbox.R

--- M - Install latest General Toolbox.R
+++ M - Install latest General Toolbox.R
@@ -3,11 +3,6 @@
 # AUTHOR:       Pieter Overdevest
 ###############################################################################
 
-# If you add functions or change the name of a function, you need to rebuild
-# the package in the toolbox project, not here in the initialization project:
-# devtools::document()      # genereer documentatie en NAMESPACE
-# devtools::install()       # installeer het pakket
-
 # If the package is attached to memory - i.e., there is a checkmark in front of
 # the package in the Packages tab - detach it from memory, first. In case the 
 # package is already detached, then this code will result in a 'invalid name 
